79b204cf26052ada3d3436dece4170a2e13cf292
UraNus0205 <787551776@qq.com>
2021-02-08 22:32:50 +0800

    2.8

---
+++ 线性表/单链表/单链表.cpp
@@ -0,0 +1,287 @@
+﻿#include <iostream>
+#include<cstdlib>
+#include<stdio.h>
+using namespace std;
+
+typedef int ElemType;
+
+
+/*
+struct LNode{
+	ElemTyoe data;
+	struct LNode *next;
+};
+typedef struct LNode LNode;//强调结点
+tyoedef struct LNode *LinkList;//强调链表
+*/
+
+typedef struct LNode {
+	ElemType data;//每个节点存放一个数据元素
+	struct LNode* next;
+}LNode,*LinkList;
+
+bool InitList1(LinkList& L);//初始化一个不带头节点的单链表
+bool InitList2(LinkList& L);//初始化一个带头结点的单链表
+bool ListInsert1(LinkList& L, int i, ElemType e);//按位序插入(带头结点)
+bool ListInsert2(LinkList& L, int i, ElemType e);//按位序插入(不带头结点)
+bool InsertNextNode(LNode* p, ElemType e);//后插操作,在p节点后插入元素e
+bool InsertPriorNode1(LNode* p, ElemType e);//前插操作,在p节点之前插入元素e
+bool InsertPriorNode2(LNode* p, LNode* s);//前插操作,在p节点之前插入节点s
+bool ListDelete(LinkList& L, int i, ElemType& e);//带头节点的按位序删除节点
+bool DeleteNode(LNode* p);//删除指定结点p
+LNode* GetElem(LinkList L, int i);//带头节点按位查找,查询第i个结点
+LNode* LocateElem(LinkList L, ElemType e);//带头节点按值查找，找到数据域等于e的结点
+int Length(LinkList L);
+bool Empty(LinkList L);
+LinkList List_TailInsert(LinkList& L);//带头节点尾插法建立单链表
+LinkList List_HeadInsert(LinkList& L);//带头节点头插法建立单链表
+
+
+
+int main()
+{
+	LinkList L;
+	InitList2(L);
+	List_HeadInsert(L);
+	cout << "链表长度为：" << Length(L) << endl;
+	return 0;
+}
+
+LNode* GetElem(LinkList L, int i)//带头节点按位查找,查询第i个结点
+{
+	int j = 0;
+	LNode* p = L;
+	if (i < 0)
+	{
+		printf("查询失败\n");
+		return NULL;
+	}
+	while (p != NULL && j < i)
+	{
+		p = p->next;
+		j++;
+	}
+	printf("查询成功\n");
+	return p;
+}
+
+LNode* LocateElem(LinkList L, ElemType e)//带头节点按值查找，找到数据域等于e的结点
+{
+	LNode* p = L->next;
+	while (p != NULL && p->data != e)
+	{
+		p = p->next;
+	}
+	printf("查找完毕\n");
+	return p;
+}
+
+
+bool InitList1(LinkList& L)//初始化一个不带头节点的单链表
+{
+	L = NULL;
+	printf("链表创建成功(不带头结点)\n");
+	return true;
+}
+
+bool InitList2(LinkList& L)//初始化一个带头结点的单链表
+{
+	L = (LNode*)malloc(sizeof(LNode));//分配一个头结点
+	if (L == NULL)//内存不足，分配失败
+	{
+		printf("内存不足，分配失败\n");
+		return false;
+	}
+	L->next = NULL;
+	printf("链表创建成功(带头结点)\n");
+	return true;
+}
+
+bool InsertNextNode(LNode* p, ElemType e)//后插操作,在p节点后插入元素e
+{
+	if (p == NULL)
+	{
+		printf("节点错误\n");
+		return false;
+	}
+	LNode* s = (LNode*)malloc(sizeof(LNode));
+	if (s == NULL)
+	{
+		printf("内存不足\n");
+		return false;
+	}
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	printf("元素%d已成功插入\n", e);
+	return true;
+}
+
+
+bool ListInsert1(LinkList& L, int i, ElemType e)//按位序插入(带头结点)
+{
+	LNode* p = GetElem(L, i - 1);
+	return InsertNextNode(p, e);
+}
+
+bool ListInsert2(LinkList& L, int i, ElemType e)//按位序插入(不带头结点)
+{
+	if (i < 1)
+	{
+		printf("i值不合法\n");
+		return false;
+	}
+	if (i == 1)
+	{
+		LNode* s = (LNode*)malloc(sizeof(LNode));
+		s->data = e;
+		s->next = L;
+		L = s;
+		printf("插入成功\n");
+		return true;
+	}
+	int j = 1;
+	LNode* p = L;
+	if (i < 0)
+	{
+		printf("查询失败\n");
+		return NULL;
+	}
+	while (p != NULL && j < i - 1)
+	{
+		p = p->next;
+		j++;
+	}
+	return InsertNextNode(p, e);
+}
+
+
+bool InsertPriorNode1(LNode* p, ElemType e)//前插操作,在p节点之前插入元素e
+{
+	if (p == NULL)
+	{
+		printf("节点错误\n");
+		return false;
+	}
+	LNode* s = (LNode*)malloc(sizeof(LNode));
+	if (s == NULL)
+	{
+		printf("内存不足\n");
+		return false;
+	}
+	s->next = p->next;
+	p->next = s;
+	s->data = p->data;
+	p->data = e;
+	printf("元素%d已成功插入\n", e);
+	return true;
+}
+
+bool InsertPriorNode2(LNode* p, LNode* s)//前插操作,在p节点之前插入节点s
+{
+	if (p == NULL || s == NULL)
+	{
+		printf("节点错误\n");
+		return false;
+	}
+
+	s->next = p->next;
+	p->next = s;
+	ElemType temp = p->data;
+	p->data = s->data;
+	s->data = temp;
+	printf("节点s已成功插入\n");
+	return true;
+}
+
+bool ListDelete(LinkList& L, int i, ElemType& e)//带头节点的按位序删除节点
+{
+	LNode* p = GetElem(L, i - 1);
+	if (p == NULL)
+	{
+		printf("i值不合法\n");
+		return false;
+	}
+	if (p->next == NULL)
+	{
+		printf("无该节点\n");
+	}
+	LNode* q = p->next;
+	e = q->data;
+	p->next = q->next;
+	free(q);
+	printf("第%d个元素%d删除成功\n", i, e);
+	return true;
+}
+
+bool DeleteNode(LNode* p)//删除指定结点p
+{
+	if (p == NULL)
+	{
+		printf("节点为空\n");
+		return false;
+	}
+	p->data = p->next->data;//若p为最后一个节点，则会出错
+	p->next = p->next->next;
+	printf("结点q删除成功\n");
+	return true;
+}
+
+
+int Length(LinkList L)
+{
+	int len = 0;
+	LNode* p = L;
+	while (p->next != NULL)
+	{
+		p = p->next;
+		len++;
+	}
+	return len;
+}
+
+bool Empty(LinkList L)//判断链表是否为空
+{
+	return (L == NULL);
+}
+
+LinkList List_TailInsert(LinkList& L)//带头节点尾插法建立单链表
+{
+	ElemType x;
+	L = (LinkList)malloc(sizeof(LNode));
+	LNode* s, * r = L;
+	cin >> x;
+	while (x != 9999)
+	{
+		s = (LNode*)malloc(sizeof(LNode));
+		s->data = x;
+		r->next = s;
+		r = s;
+		cin >> x;
+	}
+	L->next = NULL;
+	printf("尾插法建立单链表完毕\n");
+	return L;
+}
+
+LinkList List_HeadInsert(LinkList& L)//带头节点头插法建立单链表
+{
+	ElemType x;
+	L = (LinkList)malloc(sizeof(LNode));
+	LNode* s, * r = L;
+	L->next = NULL;
+	cin >> x;
+	while (x != 9999)
+	{
+		s = (LNode*)malloc(sizeof(LNode));
+		s->data = x;
+		s->next = r->next;
+		r->next = s;
+		cin >> x;
+	}
+	printf("头插法建立单链表完毕\n");
+	return L;
+}
+
+
+
---
+++ 线性表/顺序表/顺序表.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+typedef int ElemType;
+const int MaxSize1 = 100;
+const int InitSize = 10;//动态顺序表的默认最大长度
+
+typedef struct {//静态分配
+	ElemType data[MaxSize1];
+	int length;
+}SqList;
+typedef struct {//动态分配
+	ElemType* data;
+	int length;
+	int MaxSize2;
+}SeqList;
+
+void InitList1(SqList& L);
+bool ListInsert1(SqList& L, int i, ElemType e);//在第i个位置插入元素e
+bool ListDelete1(SqList& L, int i, ElemType& e);//删除第i个位置的元素存入e中
+ElemType GetElem1(SqList L, int i);//按位查找操作，返回第i个位置的元素
+int LocateElem1(SqList L, ElemType e);//按值查找操作，在L中查找第一个元素值等于e的元素，返回其位序
+
+void InitList2(SeqList& L);//初始化动态顺序表
+void IncreaseSize2(SeqList& L, int len);//增加动态数组的长度
+ElemType GetElem2(SeqList L, int i);//按位查找操作，返回第i个位置的元素
+int LocateElem2(SeqList L, ElemType e);//按值查找操作，在L中查找第一个元素值等于e的元素，返回其位序
+
+
+
+
+int main()
+{
+	SqList L1;
+	SeqList L2;
+	InitList1(L1);
+	InitList2(L2);
+
+	ListInsert1(L1, 1, 5);
+	ListInsert1(L1, 1, 6);
+	ListInsert1(L1, 2, 7);
+	ListInsert1(L1, 2, 8);
+	for (int i = 1; i <= L1.length; i++)//6875
+	{
+		printf("%d\n", GetElem1(L1, i));
+	}
+	int e;
+	ListDelete1(L1, 1, e);
+	printf("e=%d\n", e);
+	int value = 5;
+	int index = LocateElem1(L1, value);
+	printf("%d的位序为：%d\n", value, index);
+	return 0;
+}
+
+
+void InitList1(SqList &L)//初始化静态顺序表
+{
+	for (int i = 0; i < L.length; i++)
+		L.data[i] = 0;
+	L.length = 0;
+}
+
+ bool ListInsert1(SqList& L, int i, ElemType e)//在第i个位置插入元素e
+{
+	 if (i<1 || i>L.length + 1)
+	 {
+		 printf("插入位置错误\n");
+		 return false;
+	 }
+	 if (i >= MaxSize1)
+	 {
+		 printf("存储空间已满\n");
+		 return false;
+	 }
+	 for (int j = L.length; j >= i; j--)
+	 {
+		 L.data[j] = L.data[j - 1];
+	 }
+	 L.data[i - 1] = e;
+	 L.length++;
+	 printf("插入元素%d成功，此时表长为：%d\n", e, L.length);
+	 return true;
+}
+
+bool ListDelete1(SqList &L, int i, ElemType &e)//删除第i个位置的元素存入e中
+{
+	if (i<1 || i>L.length)
+	{
+		printf("删除位置错误\n");
+	}
+	e = L.data[i - 1];
+	for (int j = i; j < L.length; j++)
+	{
+		L.data[j - 1] = L.data[j];
+	}
+	L.length--;
+	printf("删除%d元素成功，此时表长为：%d\n", e, L.length);
+	return true;
+}
+
+ElemType GetElem1(SqList L, int i)//按位查找操作，返回第i个位置的元素
+{
+	return L.data[i - 1];
+}
+
+int LocateElem1(SqList L, ElemType e)//按值查找操作，在L中查找第一个元素值等于e的元素，返回其位序
+{
+	for (int i = 0; i < L.length; i++)
+	{
+		if (L.data[i] == e)
+		{
+			printf("查找元素%d成功\n", e);
+			return i + 1;
+		}
+	}
+	printf("查找元素%d失败\n", e);
+	return 0;//查找失败
+}
+
+void InitList2(SeqList& L)//初始化动态顺序表
+{
+	L.data = (ElemType*)malloc(InitSize * sizeof(ElemType));
+	if (!L.data)
+	{
+		printf("初始化失败\n");
+		exit(0);
+	}
+	printf("初始化动态顺序表成功\n");
+	L.length = 0;
+	L.MaxSize2 = InitSize;
+}
+
+void IncreaseSize2(SeqList& L, int len)//增加动态数组的长度
+{
+	int* p = L.data;
+	L.data = (ElemType*)malloc((L.MaxSize2 + len) * sizeof(ElemType));
+	for (int i = 0; i < L.length; i++)
+	{
+		L.data[i] = p[i];
+	}
+	L.MaxSize2 = L.MaxSize2 + len;
+	free(p);
+}
+
+ElemType GetElem2(SeqList L, int i)//按位查找操作，返回第i个位置的元素
+{
+	return L.data[i - 1];
+}
+
+int LocateElem2(SeqList L, ElemType e)//按值查找操作，在L中查找第一个元素值等于e的元素，返回其位序
+{
+	for (int i = 0; i < L.length; i++)
+	{
+		if (L.data[i] == e)
+		{
+			printf("查找元素%d成功\n",e);
+			return i + 1;
+		}
+	}
+	printf("查找元素%d失败\n",e);
+	return 0;//查找失败
+}
+
